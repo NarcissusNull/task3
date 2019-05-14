24da32566671972fd2e504d19e72b91daba0185e
Narcissus <1169714441@qq.com>
2019-05-14 18:44:13 +0800

    完成practice08中的所有类，并通过测试。

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,34 @@
 package practice08;
 
 public class Klass {
+    private int number;
+    private Student leader;
+
+    Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + getNumber();
+    }
+
+    public void assignLeader(Student student) {
+        setLeader(student);
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void setLeader(Student leader) {
+        this.leader = leader;
+    }
 }
