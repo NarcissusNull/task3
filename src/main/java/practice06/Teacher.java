1c7ababc28e887ed58d575209c4759f4ff1b3c7c
Narcissus <1169714441@qq.com>
2019-05-14 17:42:07 +0800

    完成practice06中的所有类，并通过测试。

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,31 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Integer klass;
+
+    public Teacher(String tom, int i, int ki) {
+        super(tom, i);
+        setKlass(ki);
+    }
+
+    public Teacher(String tom, int i) {
+        super(tom, i);
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        if (klass == null) {
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        } else {
+            return super.introduce() + " I am a Teacher. I teach Class " + klass + ".";
+        }
+    }
 }
